3b33b973ce0b34aeb3ca3c0e8d1c20a7e0eec8c6
Pitchouna19 <161199294+Pitchouna19@users.noreply.github.com>
2024-09-24 10:57:06 +0100

    Update client_report_monitor.sh

--- client_report_monitor.sh
+++ client_report_monitor.sh
@@ -7,15 +7,13 @@ MQTT_PASS="PASW"
 TOPIC="/client/monitoring"
 
 # Récupérer l'IP locale
-    IP_CLIENT=$(hostname -I | awk '{print $1}')
+IP_CLIENT=$(hostname -I | awk '{print $1}')
 
 # URLs à récupérer
 URLS=(
     "http://$IP_CLIENT:1985/api/v1/vhosts/"
-    "http://$IP_CLIENT:1985/api/v1/client/"
+    "http://$IP_CLIENT:1985/api/v1/clients/"
     "http://$IP_CLIENT:1985/api/v1/streams/"
-    "http://$IP_CLIENT:1985/api/v1/publish/"
-    "http://$IP_CLIENT:1985/api/v1/play/"
 )
 
 while true; do    
@@ -28,25 +26,33 @@ while true; do
         # Télécharger le JSON
         json_data=$(curl -s $url)
 
-        # Remplacer la valeur de "server" par $IP_CLIENT
-        json_modified=$(echo $json_data | sed "s/\"server\": *\"[^\"]*\"/\"server\": \"$IP_CLIENT\"/g")
+        # Vérifier si le JSON n'est pas vide
+        if [[ -n "$json_data" && "$json_data" != "[]" ]]; then
+            # Remplacer la valeur de "server" par $IP_CLIENT
+            json_modified=$(echo "$json_data" | sed "s/\"server\": *\"[^\"]*\"/\"server\": \"$IP_CLIENT\"/g")
 
-        # Ajouter le JSON modifié à la liste JSON
-        JSON_LIST="$JSON_LIST$json_modified,"
+            # Ajouter le JSON modifié à la liste JSON
+            JSON_LIST="$JSON_LIST$json_modified,"
+        fi
     done
 
-    # Retirer la dernière virgule et fermer la liste JSON
-    JSON_LIST=$(echo $JSON_LIST | sed 's/,$//')
+    # Retirer la dernière virgule si elle existe et fermer la liste JSON
+    JSON_LIST=$(echo "$JSON_LIST" | sed 's/,$//')
     JSON_LIST="$JSON_LIST]"
 
-    # Publier les informations sur le topic MQTT
-    mosquitto_pub -h "$BROKER_IP" -u "$MQTT_USER" -P "$MQTT_PASS" -t "$TOPIC" -m "$JSON_LIST"
+    # Si JSON_LIST est vide (juste "[]"), ne pas envoyer
+    if [ "$JSON_LIST" != "[]" ]; then
+        # Publier les informations sur le topic MQTT
+        mosquitto_pub -h "$BROKER_IP" -u "$MQTT_USER" -P "$MQTT_PASS" -t "$TOPIC" -m "$JSON_LIST"
 
-    # Afficher un message de succès
-    if [ $? -eq 0 ]; then
-        echo "Informations envoyées avec succès au broker MQTT."
+        # Afficher un message de succès
+        if [ $? -eq 0 ]; then
+            echo "Informations envoyées avec succès au broker MQTT."
+        else
+            echo "Erreur lors de l'envoi des informations au broker MQTT."
+        fi
     else
-        echo "Erreur lors de l'envoi des informations au broker MQTT."
+        echo "Aucune donnée valide à envoyer."
     fi
 
     # Attendre 4 secondes avant de répéter

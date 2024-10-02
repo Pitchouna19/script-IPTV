dccc94c1a548cea36700912fc4d36841c37c5686
Pitchouna19 <161199294+Pitchouna19@users.noreply.github.com>
2024-10-02 10:21:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -57,19 +57,8 @@ app.post('/save-xtream', (req, res) => {
             return res.status(500).send('Erreur lors de l\'enregistrement des informations');
         }
 
-        console.log('Informations enregistrées avec succès');
-        // Exécuter le script après avoir enregistré les informations
-        exec('/usr/local/bin/domaine_script.sh', (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Erreur lors de l'exécution du script: ${error.message}`);
-                return res.status(500).send("Informations enregistrées, mais échec de l'exécution du script");
-            }
-            if (stderr) {
-                console.error(`Erreur du script: ${stderr}`);
-            }
-
-            console.log(`Résultat du script: ${stdout}`);
-            res.status(200).send('Informations enregistrées et script exécuté avec succès');
+        console.log('Informations enregistrées avec succès');        
+        res.status(200).send('Informations enregistrées et script exécuté avec succès');
         });
     });
 });

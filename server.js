2f741c4a8de33ba2f62d1055c1a90e65648f45ce
Pitchouna19 <161199294+Pitchouna19@users.noreply.github.com>
2024-10-04 23:57:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { exec } = require('child_process');
+const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 

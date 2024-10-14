8e58b30bb9b7abee1245e241beff49149a661385
Pitchouna19 <161199294+Pitchouna19@users.noreply.github.com>
2024-10-14 09:07:35 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -52,7 +52,7 @@ function installation_gestremer() {
     echo "Installation GStreamer en cours..."
     sudo apt update && sudo apt upgrade -y && \
     sudo apt install -y gstreamer1.0-tools gstreamer1.0-plugins-base \
-    gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
+    gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-base-apps \
     gstreamer1.0-plugins-ugly gstreamer1.0-libav libgstreamer1.0-dev \
     libgstreamer-plugins-base1.0-dev
 

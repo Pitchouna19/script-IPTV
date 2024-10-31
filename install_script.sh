f7df449d1fa99cc0b653e70b6008b39e7a8e1842
Pitchouna19 <161199294+Pitchouna19@users.noreply.github.com>
2024-10-31 22:08:50 +0100

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -504,6 +504,7 @@ function installation_openresty_client() {
     sudo wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
     echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" \
      | sudo tee /etc/apt/sources.list.d/openresty.list
+    sudo apt update
     sudo apt install -y openresty
     echo_green "#############################################"
     echo_green "#                                           #"

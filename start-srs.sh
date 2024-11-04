79f6f065c1e616d0f7343faa7227952b74556dc4
Pitchouna19 <161199294+Pitchouna19@users.noreply.github.com>
2024-11-04 13:20:27 +0100

    Update start-srs.sh

--- start-srs.sh
+++ start-srs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -d --name srs -p 1935:1935 -p 1985:1985 -p 8080:8080 -v /usr/local/etc/srs:/usr/local/etc/srs ossrs/srs
+docker run --rm -d --name srs --network host -v /usr/local/etc/srs:/usr/local/etc/srs ossrs/srs
